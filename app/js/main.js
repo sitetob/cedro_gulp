bc780ff16f3a7afacb2c08f6437f9dbc6bfb30aa
Aneta Nosareva <admospk@mail.ru>
2024-07-24 15:13:23 +0500

    Popup menu finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
+    $('.header__btn-menu').on('click',function(){
+        $('.popup-menu').removeClass('popup-menu--close');
+    });
+    $('.popup-menu__close').on('click',function(){
+        $('.popup-menu').addClass('popup-menu--close');
+    });
 
 })
