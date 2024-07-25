960464336763c7a79f041c5156f7126164695cd1
Aneta Nosareva <admospk@mail.ru>
2024-07-25 12:36:05 +0500

    Adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -7,10 +7,29 @@ $(function () {
     });
 
     const swiperTrends = new Swiper(".trends__slider", {
-        slidesPerView: 5,
-        spaceBetween: 32,
         freeMode: true,
+        loop: true,
+        breakpoints: {
+            1920: {
+                slidesPerView: 5,
+                spaceBetween: 32,
+              },
+            1366: {
+                slidesPerView: 5,
+                spaceBetween: 32,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            530: {
+                slidesPerView: 2,
+                spaceBetween: 8,
+            },
+          },
         });
-
 });
-
